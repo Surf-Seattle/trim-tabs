e67b1661cc86c416e9a63c6dfe37f7d3e171154c
Aaron Lutz <alutz@blueorigin.com>
2021-03-14 16:18:42 -0700

    Remove ConfigureControlSurfaces
     * this was a list item on the settings screen
     * it didnt do anything and wasnt in v1.0.0 scope

--- surf/interface/baseclass/settings_screen.py
+++ surf/interface/baseclass/settings_screen.py
@@ -17,20 +17,18 @@ class SurfSettingsScreen(MDScreen):
     def on_pre_enter(self):
         if not self.list_created:
             items = [
-                "Configure Control Surfaces",
-                "Power Off",
+                ("Power Off", self.shut_down)
             ]
-            for i in items:
+            for display_text, callback in items:
                 list_item = OneLineListItem(
-                    text=i, divider="Inset", font_style="H6"
+                    text=display_text,
+                    divider="Inset",
+                    font_style="H6",
+                    on_release=callback
                 )
-                list_item.bind(on_release=self.shut_down if i == 'Power Off' else self.configure_control_surfaces)
                 self.ids._list.add_widget(list_item)
 
             self.list_created = True
-
-    def configure_control_surfaces(self, obj):
-        logger.info('UI: Configure Control Surfaces: THIS DOESNT DO ANYTHING YET')
 
     def shut_down(self, *args):
         logger.info('UI: Shutting Down')
