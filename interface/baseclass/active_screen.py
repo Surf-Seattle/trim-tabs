fa73cf7075e7eec70ff151eb5858ade76761e3c1
Aaron Lutz <alutz@blueorigin.com>
2021-05-02 13:34:18 -0700

    debugging

--- interface/baseclass/active_screen.py
+++ interface/baseclass/active_screen.py
@@ -82,7 +82,7 @@ class ControlPanel(BoxLayout):
         for tab_control_widget in self.tab_control_ids.values():
             tab_control_widget.disable()
 
-        self.screen.controller.retract_pins(blindly=True)
+        self.controller.retract_pins(blindly=True)
 
     def enable_controls(self, username: str) -> None:
         """Enable the ActiveScreen controls with values from a WaveProfile yaml file."""
@@ -95,7 +95,7 @@ class ControlPanel(BoxLayout):
         for tab_control_name, tab_control_widget in self.tab_control_ids.items():
             tab_control_widget.set_value(config['control_surfaces'][tab_control_name])
 
-        self.screen.controller.move_to(
+        self.controller.move_to(
             {
                 tab_control_name: config['control_surfaces'][tab_control_name]
                 for tab_control_name, tab_control_widget in self.tab_control_ids.items()
