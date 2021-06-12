1294eff2fd5ad4793ba073f221047253cc48d6b7
Aaron Lutz <alutz@blueorigin.com>
2021-06-12 10:09:02 -0700

    bug fixes

--- interface/baseclass/root_screen.py
+++ interface/baseclass/root_screen.py
@@ -15,7 +15,8 @@ class SurfRootScreen(MDScreen):
     def __init__(self, *args, **kwargs):
         MDScreen.__init__(self, *args, **kwargs)
         Clock.schedule_once(self.globalize_screen_manager)
-        Window.show_cursor = False
+        if os.environ.get('FULLSCREEN', "true") != "true":
+            Window.show_cursor = False
 
     def globalize_screen_manager(self, *args):
         self.screen_manager = [i for i in self.children[0].children if type(i).__name__ == 'ScreenManager'][0]
