80dc35d4675402831817e593828a4c688b35217e
Aaron Lutz <alutz@blueorigin.com>
2021-06-19 14:16:03 -0700

    debugging

--- interface/baseclass/active_screen.py
+++ interface/baseclass/active_screen.py
@@ -46,6 +46,7 @@ class SurfActiveScreen(MDScreen):
             logger.info('SurfActiveScreen.activating = True')
             u.get_root_screen(self).active_bar.refresh()
             controller.activate_profile(controller.active_profile)
+            u.get_root_screen(self).active_bar.refresh()
 
             self.activating = False
         logger.info('SurfActiveScreen.on_pre_enter.end')
