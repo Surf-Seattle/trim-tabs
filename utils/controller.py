8bfd340e5f14ebeaeeb3657c2c456854f0ce05d8
Aaron Lutz <alutz@blueorigin.com>
2021-06-20 16:38:05 -0700

    bug fix

--- utils/controller.py
+++ utils/controller.py
@@ -428,7 +428,7 @@ class Surface:
         self.retract_pin = Pin(self, 'retract', retract_pin_number)
         self.pins = [self.extend_pin, self.retract_pin]
 
-        op_modes = yaml.safe_load(open(os.path.join(CONFIG_DIR, 'operating_modes.yml'), 'r'))
+        op_modes = yaml.safe_load(open(os.path.join(CONFIG_DIR, 'operating_modes.yml'), 'r'))[self.controller.mode]
 
         self.increment_extend_duration = op_modes['incremental'][self.name]['extend']
         self.increment_retract_duration = op_modes['incremental'][self.name]['retract']
